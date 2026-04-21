fa0b68d0d11650ea88315724cc5524abc942aafe
pstolmar <pstolmar@adobe.com>
2026-04-21 15:39:11 -0700

    fix: lint blocks/hero/hero.js

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,3 +1,3 @@
 // Row 1: eyebrow, h1, lede, CTAs  Row 2: stats grid
 // No DOM restructuring needed  CSS handles the layout
-export default async function decorate() {}
+export default function decorate() {}
